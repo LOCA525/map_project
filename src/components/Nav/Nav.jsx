5213059f8f0222a3c48380043e843a124574a0c5
박준영 <pjy93525@gmail.com>
2022-07-29 16:11:50 +0900

    navBar 퍼블리싱 완료

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -4,15 +4,23 @@ const Nav = () => {
   return (
     <>
       <div className="nav">
-        <div className="logo">로고</div>
-        <div className="title">스게더</div>
-        <form action="submit">
-          <input placeholder="지역을 검색하렴 ^^"></input>
-          <button type="submit">검색!!</button>
-        </form>
-        <div className="alarm">알림</div>
-        <div className="myPage">Mypage</div>
-        <div className="login">로그인</div>
+        <div className="tab1">
+          <div className="logo">로고</div>
+          <div className="title">스게더</div>
+        </div>
+
+        <div className="search">
+          <form action="submit">
+            <input placeholder="지역을 검색하렴 ^^" className="searchInput"></input>
+            <button type="submit">검색!!</button>
+          </form>
+        </div>
+
+        <div className="tab2">
+          <div className="alarm">알림</div>
+          <div className="myPage">Mypage</div>
+          <div className="login">로그인</div>
+        </div>
       </div>
     </>
   );
