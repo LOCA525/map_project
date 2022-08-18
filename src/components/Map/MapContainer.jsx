c05ec17bb1ea65b387aa2c6c6b1d56c3bc42b525
박준영 <pjy93525@gmail.com>
2022-08-18 17:57:29 +0900

    marker 이미지 변경

--- src/components/Map/MapContainer.jsx
+++ src/components/Map/MapContainer.jsx
@@ -20,7 +20,22 @@ const MapContainer = () => {
     };
     const map = new kakao.maps.Map(container, options);
     const ps = new kakao.maps.services.Places();
+    /////////////
+    var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png", // 마커이미지의 주소입니다
+      imageSize = new kakao.maps.Size(64, 69), // 마커이미지의 크기입니다
+      imageOption = { offset: new kakao.maps.Point(27, 69) }; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
 
+    // 마커의 이미지정보를 가지고 있는 마커이미지를 생성합니다
+    var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption),
+      markerPosition = new kakao.maps.LatLng(37.54699, 127.09598); // 마커가 표시될 위치입니다
+
+    // 마커를 생성합니다
+    // var marker = new kakao.maps.Marker({
+    //   position: markerPosition,
+    //   image: markerImage, // 마커이미지 설정
+    // });
+    // marker.setMap(map);
+    ////////////
     ps.keywordSearch(place, placesSearchCB);
 
     function placesSearchCB(data, status, pagination) {
@@ -41,7 +56,10 @@ const MapContainer = () => {
       let marker = new kakao.maps.Marker({
         map: map,
         position: new kakao.maps.LatLng(place.y, place.x),
+        image: markerImage, // 마커이미지 설정
       }); // 마커에 클릭이벤트를 등록
+      marker.setMap(map);
+
       kakao.maps.event.addListener(marker, "click", function () {
         // 마커를 클릭하면 장소명이 인포윈도우에 표출
         infowindow.setContent('<div style="padding:5px;font-size:12px;">' + place.place_name + "</div>");
